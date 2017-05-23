35d26d87f46d1cc13859924fe1d836dab7f7997a
jbrownssf <jpbrown@softstackfactory.org>
2017-05-23 06:26:02 +0000

    get and post request now work, moving onto storing post request into MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 console.log('May Node be with you')
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// All your handlers here...
 
 app.listen(8080, function() {
     console.log('Listening on 8080')
@@ -17,4 +22,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
     // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
     console.log(__dirname)
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
